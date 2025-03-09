2453f88e17939f962a5050d19246e723197e8640
razevortex <razevortex@googlemail.com>
2025-03-09 17:18:35 +0100

    Update SystemJSON.ps1
    session

--- SystemJSON.ps1
+++ SystemJSON.ps1
@@ -1,8 +1,15 @@
 function SystemJSON(){
   param(
     [parameter(Mandatory=$true)]
-    [string]$path
-  )
+    [string]$path,
+    [parameter(Mandatory=$false)]
+    [string]$computername
+    )
+  # The CimSession part that should allow a remote collection of the data is still WIP and will likely not work without some further information 
+  if (-not $computername) { $session = New-CimSession }
+  else { $session = New-CimSession -ComputerName $computername}
+  
+  #  Adjust classes to Select the set of Propertys that are collected
   $classes = @{
             Bios = @('Manufacturer', 'Version')
             BaseBoard = @('Manufacturer', 'Product')
@@ -18,11 +25,12 @@ function SystemJSON(){
 
   foreach ($prop in $classes.Keys){
     $item  = @()
-    foreach ($obj in Get-CimInstance -ClassName Win32_$prop) {
+    foreach ($obj in Get-CimInstance -ClassName Win32_$prop -CimSession $session) {
         $temp = @{}
         foreach ($value in $classes.$prop){
             $temp[$value] = $obj | Select -expand $value
         }
+        # The next for lines 28:31 are a filter to not include not physical NetworkAdapter and can be removed if unwanted
         if ($prop -eq "NetworkAdapter"){
             if ($temp.PhysicalAdapter){ $item += $temp }
         }
@@ -33,5 +41,7 @@ function SystemJSON(){
 
 
   $json = $SystemObject | ConvertTo-Json
-  $json | Out-File -FilePath "$path$(Get-Date -Format 'dd_MM_yyyy_HH_mm_ss').json"
+  # note that the file is without encoding (bytes) if encoding is needed uncomment the arg
+  $json | Out-File -FilePath "$path$(Get-Date -Format 'dd_MM_yyyy_HH_mm_ss').json" #-Encoding utf-8
 }
+
